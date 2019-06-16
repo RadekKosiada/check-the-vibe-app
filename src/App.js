5f3f2eb2ec5cc21677af653535a8cd687f303391
RadekKosiada <kosiadar@gmail.com>
2019-06-16 18:10:05 +0200

    Make a post request and get response in SubmitForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SubmitForm from './components/SubmitForm';
 
--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
+import secret from "../secret.json";
 
 export default class SubmitForm extends Component {
   constructor(props) {
@@ -9,6 +10,25 @@ export default class SubmitForm extends Component {
   }
   componentDidMount() {
     console.log("did mount");
+    this.sendToAnalyze();
+  }
+  async sendToAnalyze() {
+    try {
+      const response = await fetch("https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
+      {method: "POST", 
+      headers: {
+        "Content-Type":  "application/x-www-form-urlencoded",
+        "X-RapidAPI-Host": secret["X-RapidAPI-Host"],
+        "X-RapidAPI-Key": secret["X-RapidAPI-Key"]
+      }, 
+      body: "text=great value in its price range!"
+    }
+      ); const data = await response.json();
+      this.setState({});
+      console.log(data);
+    } catch(err) {
+      console.log(err.message);
+    }
   }
   render() {
     
