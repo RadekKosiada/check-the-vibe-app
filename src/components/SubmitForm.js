0395975989c5f6db7460efa861f8bcebb45ce293
RadekKosiada <kosiadar@gmail.com>
2019-06-16 17:30:12 +0200

    Add secret and SubmitForm component

---
+++ src/components/SubmitForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class SubmitForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    
+  }
+  componentDidMount() {
+    console.log("did mount");
+  }
+  render() {
+    
+    return (
+      <div>
+ 
+      </div>
+    );
+  }
+}
