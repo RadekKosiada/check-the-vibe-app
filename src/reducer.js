a0400021a44b7a1f78d563c5bc656b18f6a50bcb
RadekKosiada <kosiadar@gmail.com>
2019-06-17 19:42:22 +0200

    Add reducer

---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+import {
+  SEND_TEXT,
+  RECEIVE_RESPONSE
+} from './actions';
+
+function checkVibe(state = initialState, action){
+  switch(action.type) {
+    case SEND_TEXT:
+      return 
+  }
+}
